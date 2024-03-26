031d91a598512668808161f116fd09fd28c4bd55
Sana Saleem <130506493+Sana-Saleem-Malik@users.noreply.github.com>
2024-03-25 17:47:02 -0700

    Update Remote_Memory_Monitor_with_SSH_and_Schedule.py
    Updated the scheduling function call to include parameters

--- Remote_Memory_Monitor_with_SSH_and_Schedule.py
+++ Remote_Memory_Monitor_with_SSH_and_Schedule.py
@@ -4,53 +4,57 @@ import paramiko
 from datetime import datetime
 
 def gather_memory_info(hostname, username, password):
-    # Current date and time
-    current_date = datetime.today().strftime("%B %d %Y")
-    current_time = datetime.now().strftime("%I:%M:%S %p")
+    try:
+        # Current date and time
+        current_date = datetime.today().strftime("%B %d %Y")
+        current_time = datetime.now().strftime("%I:%M:%S %p")
 
-    # Establish SSH connection
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=hostname, username=username, password=password)
+        # Establish SSH connection
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=hostname, username=username, password=password)
 
-    # Commands to retrieve memory information
-    available_memory_cmd = "free -g | grep Mem | awk '{print $7}'"
-    total_memory_cmd = "free -g | grep Mem | awk '{print $2}'"
-    used_memory_cmd = "free -g | grep Mem | awk '{print $3}'"
+        # Commands to retrieve memory information
+        available_memory_cmd = "free -g | grep Mem | awk '{print $7}'"
+        total_memory_cmd = "free -g | grep Mem | awk '{print $2}'"
+        used_memory_cmd = "free -g | grep Mem | awk '{print $3}'"
 
-    # Execute commands on the remote server
-    stdin, stdout, stderr = client.exec_command(available_memory_cmd)
-    available_memory = stdout.read().decode().rstrip()
-    stdin, stdout, stderr = client.exec_command(total_memory_cmd)
-    total_memory = stdout.read().decode().rstrip()
-    stdin, stdout, stderr = client.exec_command(used_memory_cmd)
-    used_memory = stdout.read().decode().rstrip()
+        # Execute commands on the remote server
+        stdin, stdout, stderr = client.exec_command(available_memory_cmd)
+        available_memory = stdout.read().decode().strip()
+        stdin, stdout, stderr = client.exec_command(total_memory_cmd)
+        total_memory = stdout.read().decode().strip()
+        stdin, stdout, stderr = client.exec_command(used_memory_cmd)
+        used_memory = stdout.read().decode().strip()
 
-    # Format memory information
-    memory_details = f"""Task:
-    Memory Detail:
-    {current_date} {current_time} and Memory details are as follows:
-    Available Memory: {available_memory} GB
-    Total Memory: {total_memory} GB
-    Used Memory: {used_memory} GB"""
+        # Format memory information
+        memory_details = f"""Task:
+        Memory Detail:
+        {current_date} {current_time} and Memory details are as follows:
+        Available Memory: {available_memory} GB
+        Total Memory: {total_memory} GB
+        Used Memory: {used_memory} GB"""
 
-    # Write memory information to a text file
-    with client.open_sftp() as sftp:
-        sftp.chdir("/home/osboxes/lec51")
-        filename = f"Memory_detail_{datetime.now().strftime('%B_%d_%Y_%I:%M:%S_%p')}.txt"
-        with sftp.open(filename, 'w') as file:
-            file.write(memory_details)
+        # Write memory information to a text file
+        with client.open_sftp() as sftp:
+            sftp.chdir("/home/osboxes/lec51")
+            filename = f"Memory_detail_{datetime.now().strftime('%B_%d_%Y_%I:%M:%S_%p')}.txt"
+            with sftp.open(filename, 'w') as file:
+                file.write(memory_details)
 
-    # Close SSH connection
-    client.close()
+        # Close SSH connection
+        client.close()
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-def schedule_memory_report():
+def schedule_memory_report(hostname, username, password):
     # Schedule memory report task to run daily
-    job1 = schedule.every().day.do(gather_memory_info)
+    job1 = schedule.every().minute.do(gather_memory_info, hostname, username, password)
 
     # Keep the scheduler running
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-schedule_memory_report()
+# Example usage:
+schedule_memory_report("192.168.100.2", "admin", "password123")
