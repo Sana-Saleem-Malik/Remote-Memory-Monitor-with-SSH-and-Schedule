14da25216e726fa62c56476355afb4cf17daa666
Sana Saleem <130506493+Sana-Saleem-Malik@users.noreply.github.com>
2024-03-25 17:34:13 -0700

    Add remote memory monitoring script
    This commit adds a Python script for monitoring memory usage on a remote server. The script utilizes SSH for remote communication, Paramiko for SSH handling, and Schedule for scheduling periodic memory reports. Memory information is retrieved from the server, formatted, and saved to a text file. The script is scheduled to run daily to gather memory usage statistics.

---
+++ Remote_Memory_Monitor_with_SSH_and_Schedule.py
@@ -0,0 +1,56 @@
+import schedule
+import time
+import paramiko
+from datetime import datetime
+
+def gather_memory_info(hostname, username, password):
+    # Current date and time
+    current_date = datetime.today().strftime("%B %d %Y")
+    current_time = datetime.now().strftime("%I:%M:%S %p")
+
+    # Establish SSH connection
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=hostname, username=username, password=password)
+
+    # Commands to retrieve memory information
+    available_memory_cmd = "free -g | grep Mem | awk '{print $7}'"
+    total_memory_cmd = "free -g | grep Mem | awk '{print $2}'"
+    used_memory_cmd = "free -g | grep Mem | awk '{print $3}'"
+
+    # Execute commands on the remote server
+    stdin, stdout, stderr = client.exec_command(available_memory_cmd)
+    available_memory = stdout.read().decode().rstrip()
+    stdin, stdout, stderr = client.exec_command(total_memory_cmd)
+    total_memory = stdout.read().decode().rstrip()
+    stdin, stdout, stderr = client.exec_command(used_memory_cmd)
+    used_memory = stdout.read().decode().rstrip()
+
+    # Format memory information
+    memory_details = f"""Task:
+    Memory Detail:
+    {current_date} {current_time} and Memory details are as follows:
+    Available Memory: {available_memory} GB
+    Total Memory: {total_memory} GB
+    Used Memory: {used_memory} GB"""
+
+    # Write memory information to a text file
+    with client.open_sftp() as sftp:
+        sftp.chdir("/home/osboxes/lec51")
+        filename = f"Memory_detail_{datetime.now().strftime('%B_%d_%Y_%I:%M:%S_%p')}.txt"
+        with sftp.open(filename, 'w') as file:
+            file.write(memory_details)
+
+    # Close SSH connection
+    client.close()
+
+def schedule_memory_report():
+    # Schedule memory report task to run daily
+    job1 = schedule.every().day.do(gather_memory_info)
+
+    # Keep the scheduler running
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+schedule_memory_report()
